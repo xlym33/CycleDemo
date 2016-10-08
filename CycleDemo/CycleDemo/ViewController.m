4b9f9a50a11af55cfc587376b9eafa5cfd7553f5
xlym33 <xlym33@sina.com>
2016-10-08 15:50:54 +0800

    修改备注

--- CycleDemo/CycleDemo/ViewController.m
+++ CycleDemo/CycleDemo/ViewController.m
@@ -21,9 +21,14 @@
     
     HSCycleView *cycleView = [[HSCycleView alloc] initWithFrame:CGRectMake(0, 100, CGRectGetWidth(self.view.frame), 250.0f) imageURLArray:@[@"1",@"2",@"3"] selectBlock:^(NSInteger index, NSString *imageURL) {
         
-        NSLog(@"~~~~~~~~~~~~~~~~%ld", index);
+        //点击了哪个item
+        NSLog(@"点击了~~~~~~~~~~~~~~~~%ld", index);
         
     } scrollBlock:^(NSInteger index) {
+        
+        //滑动到了哪个item
+        
+        NSLog(@"滑动到了~~~~~~~~~~~~~~~~%ld", index);
         
     }];
     
